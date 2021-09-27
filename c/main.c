0c6bab3fd2f08c6091fee0cf1a5309e7cd657959
StarSkyPS <91471124+StarSkyPS@users.noreply.github.com>
2021-09-27 22:46:59 +0800

    C_MB

--- c/main.c
+++ c/main.c
@@ -1,3 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-27 22:41:42
+ * @LastEditTime: 2021-09-27 22:45:20
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \undefinedc:\Users\Administrator\Desktop\c\main.c
+ */
 #include <stdio.h>
 
+int main(void)
+{
+    while(1);
+}
 
